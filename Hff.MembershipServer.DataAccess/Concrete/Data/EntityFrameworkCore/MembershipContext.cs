e4974e87b79fe8642a1c523ca4f7ad549317cabf
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-12-27 18:30:34 +0300

    Add project files.

---
+++ Hff.MembershipServer.DataAccess/Concrete/Data/EntityFrameworkCore/MembershipContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hff.MembershipServer.Service.Concrete.Data.EntityFrameworkCore
+{
+    public class MembershipContext:DbContext
+    {
+    }
+}
